75c2160ac4bac4acf03aa9c86fba8106dad83896
NatalliaMaskalenko <MoskNatulya@gmail.com>
2021-12-06 02:39:17 +0100

    Updates

---
+++ static/js/HomePage.cd2c50ee.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[0],{93:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return c}));var o=n(1);function c(){return Object(o.jsx)("div",{children:Object(o.jsx)("h1",{children:"To be always there."})})}}}]);
+//# sourceMappingURL=HomePage.cd2c50ee.chunk.js.map
