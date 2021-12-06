de9ff4adccad33e859b79beb8c1e7804ae325bcc
NatalliaMaskalenko <MoskNatulya@gmail.com>
2021-12-06 08:02:21 +0100

    Удаляет файл

--- src/redux/Phonebook/index.js
+++
@@ -1,4 +0,0 @@
-// export { default as phonebookOperations } from './phonebook-operation';
-// export { default as phonebookSelectors } from './selectors';
-// export * from './phonebook-actions';
-// export { default as phonebookReducer } from './phonebook-reducer';
